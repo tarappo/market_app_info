f9941ac143c4f4e37c71cb76b855d8e597f485cf
tarappo <tarappo@gmail.com>
2024-01-14 14:16:42 +0900

    use to_json

--- scripts/appstore.rb
+++ scripts/appstore.rb
@@ -17,7 +17,7 @@ class AppStoreMarket
     raise("No app found with the provided bundle id") if app_info.nil?
   
     release_date = Time.parse(app_info['currentVersionReleaseDate']).getlocal("+09:00")
-    formatted_date = release_date.strftime("%Y年%m月%d日 %H時%M分%S秒")  
+    formatted_date = release_date.strftime("%Y年%m月%d日 %H時%M分%S秒")
     {
       "app_name" => app_info['trackName'],
       "release_date" => formatted_date,
@@ -29,4 +29,6 @@ end
 # 'com.facebook.Facebook'
 bundle_id = ARGV[0]
 raise("Please provide a bundle id") if bundle_id.nil?
-puts AppStoreMarket.new.get_app_info(bundle_id)
+
+app_info = AppStoreMarket.new.get_app_info(bundle_id)
+puts app_info.to_json
